0c029cfc96ea93ca219b49069c336b18ee1ce6cb
Jelle Sjollema <j.sjollema@ma-web.nl>
2023-10-06 14:43:49 +0200

    centroid2

--- Assets/Centroid/CentroidManager.cs
+++ Assets/Centroid/CentroidManager.cs
@@ -10,24 +10,32 @@ public class CentroidManager : MonoBehaviour
 
     public GameObject AB;
     public GameObject AC;
+    public GameObject BC;
 
     private LineRenderer lrAB;
     private LineRenderer lrAC;
+    private LineRenderer lrBC;
 
     private LinearFunction fab = new LinearFunction(1, 1);
     private LinearFunction fac = new LinearFunction(1, 1);
+    private LinearFunction fbc = new LinearFunction(1, 1);
+
+    public GameObject hAB;
 
 
     void Start()
     {
         lrAB = AB.GetComponent<LineRenderer>();
         lrAC = AC.GetComponent<LineRenderer>();
+        lrBC = BC.GetComponent<LineRenderer>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        hAB.transform.position = new Vector3((A.transform.position.x +B.transform.position.x)/2.0f , (A.transform.position.y+ B.transform.position.y) / 2.0f, 0);
+
         fab.LineTroughTwoPoint(A.transform.position, B.transform.position);
         lrAB.SetPosition(0, new Vector3(-10,fab.GetY(-10), 0));
         lrAB.SetPosition(1, new Vector3(10, fab.GetY(10), 0));
@@ -35,6 +43,10 @@ public class CentroidManager : MonoBehaviour
         fac.LineTroughTwoPoint(A.transform.position, C.transform.position);
         lrAC.SetPosition(0, new Vector3(-10, fac.GetY(-10), 0));
         lrAC.SetPosition(1, new Vector3(10, fac.GetY(10), 0));
+
+        fbc.LineTroughTwoPoint(B.transform.position, C.transform.position);
+        lrBC.SetPosition(0, new Vector3(-10, fbc.GetY(-10), 0));
+        lrBC.SetPosition(1, new Vector3(10, fbc.GetY(10), 0));
 
     }
 }
