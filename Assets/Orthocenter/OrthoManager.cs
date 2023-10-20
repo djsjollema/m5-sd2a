29618a71656127c6b71c09227305a2697e3ddf2c
Jelle Sjollema <j.sjollema@ma-web.nl>
2023-10-20 14:45:58 +0200

    orthocenter_lineAB

--- Assets/Orthocenter/OrthoManager.cs
+++ Assets/Orthocenter/OrthoManager.cs
@@ -8,7 +8,9 @@ public class OrthoManager : MonoBehaviour
     public DragablePoint B;
     public DragablePoint C;
     private LinearFunction functionLineAB = new LinearFunction(1, 2);
+    private LinearFunction functionLine_p_AB = new LinearFunction(1, 2);
     public LineRenderer lineAB;
+    public LineRenderer line_p_AB;
 
 
     // Start is called before the first frame update
@@ -25,6 +27,9 @@ public class OrthoManager : MonoBehaviour
         lineAB.SetPosition(0, new Vector3(-20, functionLineAB.GetY(-20), 0));
         lineAB.SetPosition(1, new Vector3(20, functionLineAB.GetY(20), 0));
 
-
+        functionLine_p_AB.slope = -1 / functionLineAB.slope;
+        functionLine_p_AB.intercept = C.transform.position.y - C.transform.position.x * functionLine_p_AB.slope;
+        line_p_AB.SetPosition(0, new Vector3(-20, functionLine_p_AB.GetY(-20), 0));
+        line_p_AB.SetPosition(1, new Vector3(20, functionLine_p_AB.GetY(20), 0));
     }
 }
